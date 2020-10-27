dfe81556a0f6a249b4e0950e49dcf0ccddcae46f
JericHunter <51177847+JericHunter@users.noreply.github.com>
2020-10-27 21:32:10 +0000

    finishing iterative sorting

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -6,7 +6,14 @@ def is_sorted(items):
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
     # TODO: Check that all adjacent items are in order, return early if so
+    isSorted = False
 
+    while not isSorted:
+        for i in range(len(items)):
+            if items[i+1] < items[i]:
+                return False
+            else:
+                return True
 
 def bubble_sort(items):
     """Sort given items by swapping adjacent items that are out of order, and
@@ -15,6 +22,10 @@ def bubble_sort(items):
     TODO: Memory usage: ??? Why and under what conditions?"""
     # TODO: Repeat until all items are in sorted order
     # TODO: Swap adjacent items that are out of order
+    for i in range(len(items)):
+        if items[i] > items[i+1]:
+            items[i], items[i+1] = items[i+1], items[i]
+    return items
 
 
 def selection_sort(items):
@@ -39,3 +50,8 @@ def insertion_sort(items):
     # TODO: Repeat until all items are in sorted order
     # TODO: Take first unsorted item
     # TODO: Insert it in sorted order in front of items
+  for i in range(1,len(arr)):
+    while i > 0 and  items[i] < items[i-1]  :
+      items[i], items[i-1] = items[i-1], items[i]
+      i-=1
+  return items
